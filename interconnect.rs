18594a7b747f5401e034fa070afab30d9f36d442
WhentheWhentheWhentheWhenthe <h.dasilva1@icloud.com>
2026-05-04 17:40:01 +0100

    brought back load32

--- interconnect.rs
+++ interconnect.rs
@@ -8,4 +8,12 @@ impl Interconnect {
     pub fn new(bios: Bios) -> Interconnect {
         Interconnect { bios }
     }
+
+    pub fn load32(&self, addr: u32) -> u32 {
+        if let Some(offset) = map::BIOS.contains(addr) {
+            self.bios.load32(offset)
+        } else {
+            panic!("Unhandled load32 address: 0x{:08x}", addr)
+        }
+    }
 }

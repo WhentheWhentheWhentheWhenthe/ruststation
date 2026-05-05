f3e8584107973a6cabe0d5f9adadc332c2ae1b0b
WhentheWhentheWhentheWhenthe <h.dasilva1@icloud.com>
2026-05-05 18:37:04 +0100

    decoder
    added a decoder

--- cpu.rs
+++ cpu.rs
@@ -19,3 +19,17 @@ impl Cpu {
         self.decode_and_execute(instruction);
     }
 }
+
+impl Cpu {
+    fn decode_and_execute(&mut self, instruction: Instruction) {
+        match instruction.function() {
+            0b001111 => self.op_lui(instruction),
+            _ => panic!("Unhandled instruction_{:x}", instruction.0),
+        }
+    }
+
+    fn op_lui(&mut self, instruction: Instruction) {
+        let i = instruction.imm();
+        let t = instruction.t();
+
+        panic!("what_now?");
